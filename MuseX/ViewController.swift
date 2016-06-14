b01a3a3182629c5e3bc8ebd6a94e79e23c53a317
Will <Will@zeus.local>
2016-06-14 16:48:38 +0800

    Initial Commit

---
+++ MuseX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuseX
+//
+//  Created by Will on 6/14/16.
+//  Copyright © 2016 Will Wagers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
