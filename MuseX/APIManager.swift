d35bf23f756a37c6499ec705d8cb741e066f1473
Will <Will@zeus.local>
2016-06-15 08:45:14 +0800

    Completed Part-3

--- MuseX/APIManager.swift
+++ MuseX/APIManager.swift
@@ -11,21 +11,40 @@ import Foundation
 class APIManager {
     
     func loadData(urlString: String, completion: (result: String) -> Void) {
-        let session = NSURLSession.sharedSession()
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+//      let session = NSURLSession.sharedSession()
+        let session = NSURLSession(configuration: config)
         let url = NSURL(string: urlString)!
         
         let task = session.dataTaskWithURL(url) {
         
             (data, response, error) -> Void in
-            
-            dispatch_async(dispatch_get_main_queue()) {
+
                 if error != nil {
-                    completion(result: (error!.localizedDescription))
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: (error!.localizedDescription))
+                    }
                 } else {
-                    completion(result: "NSURLSession successful!")
-                    print(data)
+//                  print(data)
+                    do {
+                        /* .AllowFragments - top level object is NOT Array or Dictionary. Any type of string ot value. NJSONSerialization requires do-try-catch. It converts the NDData into a JSON obkect and casts it to a Dcitoinary.
+                         */
+                        if let json = try NSJSONSerialization.JSONObjectWithData(data!,
+                            options: .AllowFragments) as? [String: AnyObject] {
+                            print(json)
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue(priority, 0)) { 
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    completion(result: ("NSJSONSerialization successful!"))
+                                    }
+                                }
+                            }
+                    } catch {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completion(result: ("NSJSONSerialization failed!"))
+                        }
+                    }
                 }
-            }
         }
         task.resume()  // else stays in suspended state
     }
