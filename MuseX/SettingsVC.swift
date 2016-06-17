89084f9a5c23de900e2db04c258734043d9e4db6
Will <Will@zeus.local>
2016-06-17 11:01:33 +0800

    user defaults 1

--- MuseX/SettingsVC.swift
+++ MuseX/SettingsVC.swift
@@ -12,12 +12,21 @@ class SettingsVC: UITableViewController {
 
     @IBOutlet weak var aboutLabel: UILabel!
     @IBOutlet weak var feedbackLabel: UILabel!
-    @IBOutlet weak var securityLabel: UILabel!    
+    @IBOutlet weak var securityLabel: UILabel!
     @IBOutlet weak var securitySwitch: UISwitch!
     @IBOutlet weak var imageQualityLabel: UILabel!
     @IBOutlet weak var imageQualitySwitch: UISwitch!
     @IBOutlet weak var topXVideos: UILabel!
     @IBOutlet weak var topXVideosSlider: UISlider!
+    
+    @IBAction func touchIDSwitch(sender: UISwitch) {
+        NSUserDefaults.standardUserDefaults().setBool((securitySwitch.on ? true : false), forKey: "Settings: Security")
+    }
+    
+    @IBAction func bestImageQualitySwitch(sender: UISwitch) {
+        NSUserDefaults.standardUserDefaults().setBool((imageQualitySwitch.on ? true : false), forKey: "Settings: Best Image")
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +34,10 @@ class SettingsVC: UITableViewController {
         tableView.alwaysBounceVertical = false
 
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(preferredFontChanged), name: UIContentSizeCategoryDidChangeNotification, object: nil)
+        
+        // set defaults
+        securitySwitch.on = NSUserDefaults.standardUserDefaults().boolForKey("Settings: Security")
+        imageQualitySwitch.on = NSUserDefaults.standardUserDefaults().boolForKey("Settings: Best Image")
 
    }
     
